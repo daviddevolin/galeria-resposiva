176a15f1fe61bb88a2ac4fa1830669aec5f4c08b
daviddevolin <davidccb@live.com>
2022-07-01 11:57:01 -0300

    exibindo fotos

--- gallery/src/App.js
+++ gallery/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import Gallery from './gallery';
 
 function App() {
   return (
     <>
       <h1 style= {{textAlign: 'center'}}> Photos </h1>
+      <Gallery/>
     </>
   );
 }
